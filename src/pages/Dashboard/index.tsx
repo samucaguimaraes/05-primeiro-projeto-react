bff9412ca3b876d8398ad8280b8e5c4c9dbff002
Samuel Gomes Guimaraes <samuca.guimaraes@gmail.com>
2020-08-04 22:58:45 -0300

    - COnsumindo api do git e add na tela

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,52 +1,64 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
-
 import {Title, Form, Repositories} from './styles';
+import Repository from '../Repository';
+
+/** Não precisa a tipagem de tudo que vai ter, apenas as utilizaveis */
+interface Repository{
+  id: number;
+  name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
 
 const Dashboard: React.FC = () => {
+  /** Dados do input de pesquisa*/
+  const [newRepo, setNewRepo] = useState('');
+  /** Armazenando os reposiórios */
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+    ): Promise<void>{
+      event.preventDefault();
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+      const repository = response.data;
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+    }
+
   return (
   <>
     <img src={logoImg} alt="Github Explorer"/>
     <Title> Explore Repository no Github </Title>
 
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
+    <Form onSubmit={handleAddRepository}>
+      <input
+        value={newRepo}
+        onChange={ e => setNewRepo(e.target.value)}
+        placeholder="Digite o nome do repositório"
+      />
       <button type="submit">Pesquisar</button>
     </Form>
     <Repositories>
-      <a href="teste">
-        <img src="https://avatars1.githubusercontent.com/u/3595816?s=400&u=da4daf60f5c320ca5a5cae87258301d2efa907f1&v=4"
-        alt="Samuel Guimarães"
+      {repositories.map(repository => (
+      <a key={repository.id}  href="teste">
+        <img src={repository.owner.avatar_url}
+        alt={repository.owner.login}
         />
         <div>
-          <strong>gostack-conceitos-node</strong>
-          <p>Aplicação para treinar o que você aprendeu até agora no Node.js!</p>
+          <strong>{repository.name}</strong>
+          <p>{repository.description}</p>
         </div>
         <FiChevronRight size={20} />
       </a>
-      <a href="teste">
-        <img src="https://avatars1.githubusercontent.com/u/3595816?s=400&u=da4daf60f5c320ca5a5cae87258301d2efa907f1&v=4"
-        alt="Samuel Guimarães"
-        />
-        <div>
-          <strong>gostack-conceitos-node</strong>
-          <p>Aplicação para treinar o que você aprendeu até agora no Node.js!</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-      <a href="teste">
-        <img src="https://avatars1.githubusercontent.com/u/3595816?s=400&u=da4daf60f5c320ca5a5cae87258301d2efa907f1&v=4"
-        alt="Samuel Guimarães"
-        />
-        <div>
-          <strong>gostack-conceitos-node</strong>
-          <p>Aplicação para treinar o que você aprendeu até agora no Node.js!</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
+      ))}
     </Repositories>
   </>
   )
